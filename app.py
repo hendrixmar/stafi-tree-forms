c98a61b0a1d110947d142f6d63e6609e0f8b546a
hendrixmar <hendrikuwabara@gmail.com>
2024-04-06 11:36:19 -0600

    streamlit app working

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
+import json
+
 import streamlit as st
-import pandas as pd
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
-###################################
-from st_aggrid import AgGrid
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-from st_aggrid.shared import JsCode
-
-###################################
-
-from functionforDownloadButtons import download_button
-
-###################################
+from main import create_tree
 
 
 def _max_width_():
@@ -26,57 +19,42 @@ def _max_width_():
         unsafe_allow_html=True,
     )
 
-st.set_page_config(page_icon="✂️", page_title="CSV Wrangler")
 
-# st.image("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/balloon_1f388.png", width=100)
+st.set_page_config(page_icon="✂️", page_title="Zoho to zingtree converter")
 st.image(
     "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/scissors_2702-fe0f.png",
     width=100,
 )
 
-st.title("CSV Wrangler")
-
-# st.caption(
-#     "PRD : TBC | Streamlit Ag-Grid from Pablo Fonseca: https://pypi.org/project/streamlit-aggrid/"
-# )
-
-
-# ModelType = st.radio(
-#     "Choose your model",
-#     ["Flair", "DistilBERT (Default)"],
-#     help="At present, you can choose between 2 models (Flair or DistilBERT) to embed your text. More to come!",
-# )
-
-# with st.expander("ToDo's", expanded=False):
-#     st.markdown(
-#         """
-# -   Add pandas.json_normalize() - https://streamlit.slack.com/archives/D02CQ5Z5GHG/p1633102204005500
-# -   **Remove 200 MB limit and test with larger CSVs**. Currently, the content is embedded in base64 format, so we may end up with a large HTML file for the browser to render
-# -   **Add an encoding selector** (to cater for a wider array of encoding types)
-# -   **Expand accepted file types** (currently only .csv can be imported. Could expand to .xlsx, .txt & more)
-# -   Add the ability to convert to pivot → filter → export wrangled output (Pablo is due to change AgGrid to allow export of pivoted/grouped data)
-# 	    """
-#     )
-# 
-#     st.text("")
-
+st.title("Zoho to zingtree converter")
 
 c29, c30, c31 = st.columns([1, 6, 1])
 
 with c30:
-
-    uploaded_file = st.file_uploader(
-        "",
-        key="1",
+    uploaded_file: list[UploadedFile] = st.file_uploader(
+        "asdds",
+        key="2",
         help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
+        accept_multiple_files=True
     )
 
-    if uploaded_file is not None:
-        file_container = st.expander("Check your uploaded .csv")
-        shows = pd.read_csv(uploaded_file)
-        uploaded_file.seek(0)
-        file_container.write(shows)
+    if len(uploaded_file) >= 1:
+        files = {element.type: element for element in uploaded_file}
 
+        if 'text/csv' in files:
+            file_container = st.expander("Check your uploaded .csv")
+
+            result = create_tree(
+                files.get('text/csv'),
+                files.get('application/json')
+            )
+            href = st.download_button(data=json.dumps(result), file_name='zintree_form.json', label='Download')
+        else:
+            st.info(
+                f"""
+                 Select a valid Json and CSV file
+                """
+            )
     else:
         st.info(
             f"""
@@ -85,53 +63,3 @@ with c30:
         )
 
         st.stop()
-
-from st_aggrid import GridUpdateMode, DataReturnMode
-
-gb = GridOptionsBuilder.from_dataframe(shows)
-# enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
-gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
-gb.configure_selection(selection_mode="multiple", use_checkbox=True)
-gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
-gridOptions = gb.build()
-
-st.success(
-    f"""
-        💡 Tip! Hold the shift key when selecting rows to select multiple rows at once!
-        """
-)
-
-response = AgGrid(
-    shows,
-    gridOptions=gridOptions,
-    enable_enterprise_modules=True,
-    update_mode=GridUpdateMode.MODEL_CHANGED,
-    data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-    fit_columns_on_grid_load=False,
-)
-
-df = pd.DataFrame(response["selected_rows"])
-
-st.subheader("Filtered data will appear below 👇 ")
-st.text("")
-
-st.table(df)
-
-st.text("")
-
-c29, c30, c31 = st.columns([1, 1, 2])
-
-with c29:
-
-    CSVButton = download_button(
-        df,
-        "File.csv",
-        "Download to CSV",
-    )
-
-with c30:
-    CSVButton = download_button(
-        df,
-        "File.csv",
-        "Download to TXT",
-    )
---
+++ main.py
@@ -0,0 +1,106 @@
+import csv
+import math
+import random
+import json
+
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+
+
+def search_button_node(nodes: dict[str, dict], node_id: str) -> dict:
+    for node_key, node in nodes.items():
+        for _, button in node["buttons"].items():
+            if button["button_link"] == node_id:
+                yield node
+
+
+def search_node(nodes: dict[str, dict], node_id: str) -> dict | None:
+    return nodes.get(node_id)
+
+
+def next_node_to_connect__(next_node: str, tree: dict[str, dict], nodes_settings: dict) -> str:
+    if (node_found := nodes_settings.get(next_node)) and node_found['include'] == '1':
+        return next_node
+
+    if next_node in tree['nodes']:
+        temp_node = tree['nodes'][next_node]["buttons"]["0"]["button_link"]
+        del tree['nodes'][next_node]
+
+        next_node_to_connect__(temp_node, tree, nodes_settings)
+
+
+def delete_node(nodes: dict[str, dict], node_id: str):
+    if node_id not in nodes['nodes']:
+        return
+
+    del nodes['nodes'][node_id]
+
+
+def next_node_to_connect(next_node: str, tree: dict[str, dict], nodes_settings: dict) -> str:
+    if (node_found := nodes_settings.get(next_node)) and node_found['include']:
+        return next_node
+    elif next_node not in nodes_settings:
+        return next_node
+
+    if next_node in tree['nodes']:
+        temp_node = tree['nodes'][next_node]["buttons"]["0"]["button_link"]
+        return next_node_to_connect(temp_node, tree, nodes_settings)
+
+
+# get the node that should no be included
+# find the next node of that node that is included and delete all the childs that are not included
+# find out the node that have that node
+
+
+# test_2()
+
+
+def load_settings(file_uploaded: UploadedFile):
+    positions = [
+        None, None, None, 39, 40, 41,
+        42, 70, 44, 47, 48, 49, 50, 54,
+        55, 56, 57, 58, 60, 62, 64, 65,
+        66, 67, 51, 52, 61, 63, 68
+    ]
+    nodes = {i: str(e) for i, e in enumerate(positions) if e is not None}
+    settings = {}
+
+    import pandas as pd
+
+    data = pd.read_csv(file_uploaded)
+    for index, col in enumerate(data.iloc[0]):
+        if isinstance(col, str):
+            settings[nodes[index]] = {'include': bool(col == 'TRUE'), 'node': nodes[index]}
+
+    return settings
+
+
+def create_tree(zoho_report: UploadedFile, zingtree_json: UploadedFile | None = None):
+
+    if zingtree_json is None:
+        with open('example.json', 'r') as fp:
+            zingtree = json.load(fp)
+    else:
+        zingtree = json.loads(zingtree_json.read())
+
+    #with open(file_name.name, 'r') as fp:
+    #    settings = {row['node']: row for row in csv.DictReader(fp)}
+    settings = load_settings(zoho_report)
+    node_deletes = []
+    for node_ide, setting in settings.items():
+        if not setting['include']:
+            result = next_node_to_connect(setting['node'], zingtree, settings)
+            node_deletes.append(node_ide)
+            for node_button in search_button_node(zingtree["nodes"], setting['node']):
+                # delete_node(zingtree, setting['node'])
+                if node_button:
+                    project = node_button['project_node_id']
+                    for key in zingtree["nodes"][project]["buttons"]:
+                        button = zingtree["nodes"][project]["buttons"][key]
+                        button['button_link'] = result
+
+    for node in node_deletes:
+        delete_node(zingtree, node)
+
+    return zingtree
+
+
